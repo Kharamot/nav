f841561282ca23d1156a6192d1990b8f224af214
Kharamot <jaggression@gmail.com>
2017-03-26 18:54:30 -0700

    Init

--- nav/ViewController.swift
+++ nav/ViewController.swift
@@ -8,8 +8,19 @@
 
 import UIKit
 
+class MyInt {
+    var x: Int = 0
+}
+
 class ViewController: UIViewController {
 
+    var myInt1 = MyInt()
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print("1st: myInt1 = \(myInt1.x)")
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +31,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if(segue.identifier == "fromOneToTwo")
+        {
+            let vc = segue.destination as! SecondViewController
+            vc.myInt2 = myInt1
+        }
+    }
 }
 
